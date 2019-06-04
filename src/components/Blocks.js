bade9842e43430d860eedacd72bb6f61a7e8961f
tdj120 <tdj120@gmail.com>
2019-06-03 21:47:53 -0700

    updated file structure, added some js

---
+++ src/components/Blocks.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ImageBox(props) {
+  return (
+    <div className="card">
+      <div className="img-container">
+        <img alt={props.name} src={props.image} />
+      </div>
+    </div>
+  );
+}
+  
+  export default ImageBox;
